773150a2b53f194c841ec2d9563d87f706e2026d
Chinthaka Jayarathne <jgcjayarathne@gmail.com>
2019-07-26 16:53:21 +0530

    EEPROM Sucessfull
    Update enum states
    updated enum off conditions
    update struct CtoM and MtoC(special attention to matching sizes in C and M codes)
    Read last off condition from EEPROM on startup
    Write last off condition to EEPROM when turning off

--- Debug/subdir_rules.mk
+++ Debug/subdir_rules.mk
@@ -8,7 +8,7 @@ SHELL = cmd.exe
 %.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: ARM Compiler'
-	"C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/bin/armcl" -mv7M3 -g --gcc --define=ccs --include_path="C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include" --include_path="C:/ti/controlSUITE/device_support/f28m35x/v100/MWare" --diag_warning=225 -me --gen_func_subsections --abi=eabi --code_state=16 --ual --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.5.LTS/bin/armcl" -mv7M3 --code_state=16 -me --include_path="C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.5.LTS/include" --include_path="C:/ti/controlSUITE/device_support/f28m35x/v100/MWare" --define=ccs -g --gcc --diag_warning=225 --gen_func_subsections=on --abi=eabi --ual --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
---
+++ memoryAddress.h
@@ -0,0 +1,17 @@
+/*
+ * memoryAddress.h
+ *
+ *  Created on: Jul 21, 2019
+ *      Author: Chinthaka
+ *
+ *      This file contains memory address of the parameter saved on the external EEPROM
+ */
+
+#ifndef MEMORYADDRESS_H_
+#define MEMORYADDRESS_H_
+
+const unsigned short LAST_OFF_CONDITION_ADDR = 0x0000;
+
+
+
+#endif /* MEMORYADDRESS_H_ */
